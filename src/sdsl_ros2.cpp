1aa96eb66ede0102550e40272cc72cef38e3a0de
Michael Bilevich <mickelbil84@gmail.com>
2025-08-25 19:04:25 +0300

    Convert occupany grid to sdsl map

--- src/sdsl_ros2.cpp
+++ src/sdsl_ros2.cpp
@@ -2,6 +2,14 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
+
+#define SDSL_CPP_ONLY
+#include <sdsl.hpp>
+#include <environments/env_R3_pcd.hpp>
+#include <CGAL/Simple_cartesian.h>
+using Kernel = CGAL::Simple_cartesian<double>;
+using FT = Kernel::FT;
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -13,9 +21,36 @@ class SDSL_ROS2 : public rclcpp::Node {
 public:
     SDSL_ROS2() :
         Node("sdsl_ros2") {
+        mapSubscription_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+            "map", 10, std::bind(&SDSL_ROS2::mapCallback, this, std::placeholders::_1));
     }
 
 private:
+    // Subscriptions and publishers
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr mapSubscription_;
+
+    // SDSL structures
+    sdsl::Env_R3_PCD<Kernel> environment_;
+
+    // ---------------------------------------------------------------------------------------------
+    void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+        RCLCPP_INFO(this->get_logger(), "Received map with size: %d x %d", msg->info.width, msg->info.height);
+
+        std::vector<Kernel::Point_3> points;
+        for (int y = 0; y < msg->info.height; ++y)
+        for (int x = 0; x < msg->info.width; ++x) {
+            int idx = x + y * msg->info.width;
+            int8_t cell = msg->data[idx];
+            if (cell < 0) continue;
+            if (cell > 20) continue; // TODO: Move the hardcoded threshold to a parameter
+            double wx = msg->info.origin.position.x + (x + 0.5) * msg->info.resolution;
+            double wy = msg->info.origin.position.y + (y + 0.5) * msg->info.resolution;
+            Kernel::Point_3 pt(wx, wy, 0.0);
+            points.push_back(pt);
+        }
+        environment_ = sdsl::Env_R3_PCD<Kernel>(points);
+        RCLCPP_INFO(this->get_logger(), "Environment constructed with %zu points", points.size());
+    }
 };
 
 int main(int argc, char** argv) {
