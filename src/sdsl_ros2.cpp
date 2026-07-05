dc57f7a4df9ef8a3f17ac41a2624f268349a75de
Michael Bilevich <mickelbil84@gmail.com>
2026-07-05 15:25:24 +0300

    Fix typo

--- src/sdsl_ros2.cpp
+++ src/sdsl_ros2.cpp
@@ -9,7 +9,7 @@
 #include <sdsl/sdsl.hpp>
 #include <sdsl/environments/env_pgm.hpp>
 #include <sdsl/predicates/pred_forward_2d.hpp>
-#include <CGAL/Simple_caretsian.h>
+#include <CGAL/Simple_cartesian.h>
 using Kernel = CGAL::Simple_cartesian<double>;
 using FT = Kernel::FT;
 
